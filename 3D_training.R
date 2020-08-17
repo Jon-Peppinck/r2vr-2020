bd4468ce7e92d9a6c6d91e3b783eb2e3baa97306
Jon-Peppinck <mrjonpeppinck@gmail.com>
2020-08-17 13:28:37 +0700

    Implemented 'look-at': Markers always rotated to camera

--- 3D_training.R
+++ 3D_training.R
@@ -3,9 +3,9 @@ library(r2vr)
 # Find the user's IP address as it is required for WebSocket connection
 IPv4_ADDRESS <- find_IP() 
 
-## 3D image paths
+## 3D image paths (2400x1200)
 img_paths <- list(
-  list(img = "./inst/ext/images/reef/1000030039.jpg"),
+  # list(img = "./inst/ext/images/reef/1000030039.jpg"),
   list(img = "./inst/ext/images/reef/120261897.jpg"),
   list(img = "./inst/ext/images/reef/130030287.jpg"),
   list(img = "./inst/ext/images/reef/130050093.jpg")
@@ -36,7 +36,8 @@ for (i in 1:length(img_paths)) {
 canvas_3d <- a_entity(
   .tag = "sky",
   .js_sources = list(
-    "./inst/js/button_controls.js", 
+    "./inst/js/button_controls.js",
+    # "https://unpkg.com/aframe-look-at-component@0.5.1/dist/aframe-look-at-component.min.js",
     "./inst/js/bundle3d.js"
   ),
   id = "canvas3d",
@@ -52,6 +53,8 @@ canvas_3d <- a_entity(
 # Create a cursor
 cursor <- a_entity(
   .tag = "cursor",
+  camera = "",
+  look_controls = "",
   color = "#ff0000"
 )
 
@@ -59,20 +62,86 @@ cursor <- a_entity(
 camera <- a_entity(
   .tag = "camera",
   .children = list(cursor),
-  position = c(0, 0, 0)
+  position = c(0, 0, 0),
+  cursor = "rayOrigin: mouse" # note: entity works
 )
 
-### RENDER SCENE
+## Markers
 list_of_children_entities <- list(canvas_3d, camera)
+
+initial_list_length <- length(list_of_children_entities)
+
+for (i in 1:50) {
+  sphere_radius = 500
+  # TODO: exclude 0?
+  # x <- runif(1, -1, 1) 
+  # y <- runif(1, -1, 1) 
+  # z <- runif(1, -1, 1)
+  
+  u <- runif(1, -1, 1)
+  theta <- runif(1, 0, 2*pi)
+  
+  x <- sqrt(1 - u^2) * cos(theta)
+  y <- sqrt(1 - u^2) * sin(theta)
+  z <- u
+  
+  r_mag <- sqrt(x^2 + y^2 + z^2)
+  
+  angle1 <- atan2(y, x)
+  angle2 <- asin(-u/(r_mag))
+  
+  angle1_deg <- angle1 * (180/pi)
+  angle2_deg <- angle2 * (180/pi)
+  
+  
+  # v <- runif(1)
+  # theta <- 2 * pi * u
+  # phi <- acos((2 * v) -1)
+  
+  # x_normal = x/(x^2 + y^2 + z^2)
+  # y_normal = y/(x^2 + y^2 + z^2)
+  # z_normal = z/(x^2 + y^2 + z^2)
+  # 
+  # x_ring = sphere_radius * x_normal
+  # y_ring = sphere_radius * y_normal
+  # z_ring = sphere_radius * z_normal
+  
+  ring <- a_entity(
+    .tag = "ring",
+    position = c(x, y, z),
+    # rotation = c(angle1_deg, angle2_deg, 0),
+    radius_outer = 0.04,
+    radius_inner = 0.03,
+    color = "#FF0000",
+    side = "double",
+    look_at = "[cursor]"
+  )
+  marker_i <- paste0("ring", i)
+  list_of_children_entities[[initial_list_length + i]] <- assign(marker_i, ring)
+}
+
+
+
+box1 <- a_entity(
+  .tag = "box",
+  class = "box",
+  position = c(1, 0, -4),
+  color = "orange",
+  cursor_listener = "",
+  laser_controls="hand:right",
+  btn_down = ""
+)
+
+
+### RENDER SCENE
+# list_of_children_entities <- list(list_of_children_entities)
 
 animals <- a_scene(
   .children = list_of_children_entities,
   .websocket = TRUE,
   .websocket_host = IPv4_ADDRESS,
   .template = "empty",
-  button_controls = "debug: true;",
-  coral_cover_2d_buttons = "",
-  intersection = ""
+  button_controls = "debug: true;"
 )
 
 ### FUNCTIONS ###
@@ -85,4 +154,11 @@ start <- function(){
 ## End the server
 end <- function(){
   a_kill_all_scenes()
+}
+
+## Restart the server with file changes
+restart <- function(){
+  a_kill_all_scenes()
+  source('C:/r2vr2020/r2vr/3D_training.R', echo=TRUE)
+  animals$serve(host = IPv4_ADDRESS)
 }
--- inst/js/bundle3d.js
+++ inst/js/bundle3d.js
@@ -1,1 +1,101 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="/dist/js/",n(n.s=42)}({42:function(e,t,n){"use strict";console.log(3,"index3d.ts")}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/dist/js/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./typescript/3d/index.ts");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./typescript/3d/index.ts":
+/*!********************************!*\
+  !*** ./typescript/3d/index.ts ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\r\n// @ts-nocheck\r\n// https://unpkg.com/aframe-look-at-component@0.5.1/index.js\r\nvar debug = AFRAME.utils.debug;\r\nvar coordinates = AFRAME.utils.coordinates;\r\nvar warn = debug('components:look-at:warn');\r\nvar isCoordinates = coordinates.isCoordinate || coordinates.isCoordinates;\r\ndelete AFRAME.components['look-at'];\r\n/**\r\n * Look-at component.\r\n *\r\n * Modifies rotation to either track another entity OR do a one-time turn towards a position\r\n * vector.\r\n *\r\n * If tracking an object via setting the component value via a selector, look-at will register\r\n * a behavior to the scene to update rotation on every tick.\r\n */\r\nAFRAME.registerComponent('look-at', {\r\n    schema: {\r\n        default: '',\r\n        parse: function (value) {\r\n            // A static position to look at.\r\n            if (isCoordinates(value) || typeof value === 'object') {\r\n                return coordinates.parse(value);\r\n            }\r\n            // A selector to a target entity.\r\n            return value;\r\n        },\r\n        stringify: function (data) {\r\n            if (typeof data === 'object') {\r\n                return coordinates.stringify(data);\r\n            }\r\n            return data;\r\n        },\r\n    },\r\n    init: function () {\r\n        this.target3D = null;\r\n        this.vector = new THREE.Vector3();\r\n    },\r\n    /**\r\n     * If tracking an object, this will be called on every tick.\r\n     * If looking at a position vector, this will only be called once (until further updates).\r\n     */\r\n    update: function () {\r\n        var self = this;\r\n        var target = self.data;\r\n        var object3D = self.el.object3D;\r\n        var targetEl;\r\n        // No longer looking at anything (i.e., look-at=\"\").\r\n        if (!target ||\r\n            (typeof target === 'object' && !Object.keys(target).length)) {\r\n            return self.remove();\r\n        }\r\n        // Look at a position.\r\n        if (typeof target === 'object') {\r\n            return object3D.lookAt(new THREE.Vector3(target.x, target.y, target.z));\r\n        }\r\n        // Assume target is a string.\r\n        // Query for the element, grab its object3D, then register a behavior on the scene to\r\n        // track the target on every tick.\r\n        targetEl = self.el.sceneEl.querySelector(target);\r\n        if (!targetEl) {\r\n            warn('\"' + target + '\" does not point to a valid entity to look-at');\r\n            return;\r\n        }\r\n        if (!targetEl.hasLoaded) {\r\n            return targetEl.addEventListener('loaded', function () {\r\n                self.beginTracking(targetEl);\r\n            });\r\n        }\r\n        return self.beginTracking(targetEl);\r\n    },\r\n    tick: function (t) {\r\n        // Track target object position. Depends on parent object keeping global transforms up\r\n        // to state with updateMatrixWorld(). In practice, this is handled by the renderer.\r\n        var target;\r\n        var target3D = this.target3D;\r\n        var object3D = this.el.object3D;\r\n        var vector = this.vector;\r\n        if (target3D) {\r\n            target = object3D.parent.worldToLocal(target3D.getWorldPosition());\r\n            if (this.el.getObject3D('camera')) {\r\n                // Flip the vector to -z, looking away from target for camera entities. When using\r\n                // lookat from THREE camera objects, this is applied for you, but since the camera is\r\n                // nested into a Object3D, we need to apply this manually.\r\n                vector.subVectors(object3D.position, target).add(object3D.position);\r\n            }\r\n            else {\r\n                vector = target;\r\n            }\r\n            object3D.lookAt(vector);\r\n        }\r\n    },\r\n    beginTracking: function (targetEl) {\r\n        this.target3D = targetEl.object3D;\r\n    },\r\n});\r\nAFRAME.registerComponent('btn-down', {\r\n    init: function () {\r\n        var _this = this;\r\n        console.log(8, 'btn-down registered');\r\n        var controlsEl = document.querySelector('[button-controls]');\r\n        // Detect buttons selected in WebVR\r\n        controlsEl.addEventListener('buttondown', function (e) {\r\n            var el = _this.el;\r\n            if (el.className === 'box')\r\n                console.log(88, e.target, el, el.className);\r\n            _this.el.setAttribute('material', 'color', 'red');\r\n        });\r\n    },\r\n});\r\n\n\n//# sourceURL=webpack:///./typescript/3d/index.ts?");
+
+/***/ })
+
+/******/ });
