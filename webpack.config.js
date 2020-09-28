ab2e0fb3bf8dc9023b0e463e997ec7c7bb121977
Jon-Peppinck <mrjonpeppinck@gmail.com>
2020-09-28 21:45:54 +0700

    Webpack 2D (re-done) Training and Testing configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ module.exports = {
   context: __dirname,
   entry: {
     bundle2d: './typescript/2d/index.ts',
+    training2d: './typescript/2dNew/training2d.ts',
+    testing2d: './typescript/2dNew/testing2d.ts',
     training3d: './typescript/3d/training3d.ts',
     testing3d: './typescript/3d/testing3d.ts',
   },
