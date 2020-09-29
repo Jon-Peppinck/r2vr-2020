8a98331b22be03226c465a4c8492320ecfd07be8
Jon-Peppinck <mrjonpeppinck@gmail.com>
2020-09-29 17:13:50 +0700

    2D Module - Redux - annotations, evaluation, image

--- typescript/2dNew/store/models/actions.ts
+++ typescript/2dNew/store/models/actions.ts
@@ -1,4 +1,12 @@
+import { AnnotationActionTypes } from '../annotation/models/actions';
+import { EvaluationActionTypes } from '../evaluation/models/actions';
+import { ImageActionTypes } from '../image/models/actions';
 import { MetaDataActionTypes } from '../metadata/models/actions';
 import { UserActionTypes } from '../user/models/actions';
 
-export type AppActions = MetaDataActionTypes | UserActionTypes;
+export type AppActions =
+  | AnnotationActionTypes
+  | EvaluationActionTypes
+  | ImageActionTypes
+  | MetaDataActionTypes
+  | UserActionTypes;
--- typescript/2dNew/store/rootStore.ts
+++ typescript/2dNew/store/rootStore.ts
@@ -2,12 +2,18 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk, { ThunkMiddleware } from 'redux-thunk';
 import { logger } from 'redux-logger';
 
+import { annotationReducer } from './annotation/AnnotationReducer';
+import { evaluationReducer } from './evaluation/EvaluationReducer';
+import { imageReducer } from './image/ImageReducer';
 import { metaDataReducer } from './metadata/MetaDataReducer';
 import { userReducer } from './user/UserReducer';
 
 import { AppActions } from './models/actions';
 
 const rootReducer = combineReducers({
+  annotationReducer,
+  evaluationReducer,
+  imageReducer,
   metaDataReducer,
   userReducer,
 });
